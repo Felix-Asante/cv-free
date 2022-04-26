4b7160743779741ba5f991269014010c7b88cb0c
Felix-Asante <quamzgraphix@gmail.com>
2022-04-26 23:32:40 +0000

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+	return (
+		<header>
+			<h2 className="app-name">
+				CV <span className="text-success">Free</span>
+			</h2>
+		</header>
+	);
+}
+
+export default Header;

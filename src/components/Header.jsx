e23b3db3449b3defa3112fa0a30130f6543c1a83
Felix-Asante <quamzgraphix@gmail.com>
2022-04-27 00:05:38 +0000

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import style from "../styles/header.module.css";
 
 function Header() {
 	return (
-		<header>
-			<h2 className="app-name">
+		<header className={style.header}>
+			<h2 className={style["app-name"]}>
 				CV <span className="text-success">Free</span>
 			</h2>
 		</header>
